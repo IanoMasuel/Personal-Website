fd1cf4119a787f3a719311334452af8658506215
iano <samuel.soriano@hotmail.com>
2018-10-16 18:59:27 -0600

    Added skills page

--- final_web/js/inScrl-init.js
+++ final_web/js/inScrl-init.js
@@ -1,6 +1,8 @@
-$('.container').infiniteScroll({
-    // options
-    path: 'page2.html',
-    append: '.post',
-    history: false,
-  });
+$( document ).ready(function() {
+    $(".skill-list-block").hide();
+    $( ".grid-1-skills").click(function( event ) {
+    
+        $(".skill-list-block").slideToggle(500);
+    });            
+ 
+});
